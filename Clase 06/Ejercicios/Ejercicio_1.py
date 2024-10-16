d146a490c5abfc9193f3e19726630d4ce4a6b550
titojp <titojp2002@hotmail.com>
2024-10-16 17:41:08 -0300

    prep after class

---
+++ Clase 06/Ejercicios/Ejercicio_1.py
@@ -0,0 +1,8 @@
+"""
+Control de stock de productos
+
+Desarrollá un programa que permita al usuario ingresar el nombre de varios productos 
+y la cantidad en stock que hay de cada uno. El programa debe seguir pidiendo que ingrese 
+productos hasta que el usuario decida salir, ingresando "salir" como nombre de producto. 
+Después de que el bucle termine, el programa debe mostrar la cantidad total de productos ingresados.  
+"""
---
+++ Clase 06/Ejercicios/Ejercicio_2.py
@@ -0,0 +1,16 @@
+"""
+Validación de precios de productos
+
+Escribí un programa que permita al usuario ingresar el precio de un producto, 
+pero que sólo acepte valores mayores a 0. Si el usuario ingresa un valor inválido (negativo o cero), 
+el programa debe mostrar un mensaje de error, y volver a pedir el valor hasta que se ingrese uno válido. 
+Al final, el programa debe mostrar el precio aceptado.  
+
+"""
+
+precio_producto = float(input("Ingrese el precio del producto: "))
+while precio_producto <= 0:
+    print("Error: El precio no puede ser menor o igual a 0.")
+    precio_producto = float(input("\nIngrese el precio del producto: "))
+
+print(f"El precio ingresador fue {precio_producto}")
