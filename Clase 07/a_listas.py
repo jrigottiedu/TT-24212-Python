8483afafb5e3a1698830a69f89db762bfc286ce5
Juan Pablo Rigotti <juan.rigotti@bue.edu.ar>
2024-10-21 23:39:01 -0300

    clase 07

--- Clase 07/a_listas.py
+++ Clase 07/a_listas.py
@@ -2,7 +2,7 @@
 Colecciones / Arreglos
 
 LISTAS
-
+"""
 
 # Declaracion tipo de datos simples
 numero = None
@@ -17,29 +17,29 @@ print(lista)
 
 print(lista[1])  # imprime el primer elemento de la lista
 
-print()
-
+# Usamos un bucle while para iterar los elementos de la lista
 indice = 0
 while indice < len(lista):  # len(lista) nos devuelve la longitud => 3
     # print(lista[indice])
     print(f"lista[{indice}]: {lista[indice]}")
     indice += 1
 
-    """
 
-# estructua 0: producto, 1: cantidad, 2: bool
+# Armamos dos listas con la siguiente estructura:
+# posición 0: producto, 1: cantidad, 2: bool (para indicar si el producto fue comprado o no)
 elemento_0 = ["manzana", 10, False]
 elemento_1 = ["pera", 5, False]
 
-# Armar una lista de lista
+# Armamos una lista de listas
 # Lista de productos a comprar
 lista_super = [elemento_0, elemento_1]
 # print(lista_super)
 
-"""
-iteramos la lista_super de forma básica
+
+# Iteramos la lista_super de forma básica
 indice = 0  # declaro el contador / indice
-while indice < len(lista_super): #len(lista_super) => 2
+while indice < len(lista_super):  # len(lista_super) => 2
+    # Imprimo la sub-lista sin formato
     print(lista_super[indice])
     indice += 1
 
@@ -48,9 +48,23 @@ while indice < len(lista_super): #len(lista_super) => 2
 indice = 0  # declaro el contador / indice
 while indice < len(lista_super):  # len(lista_super) => 2
     elemento = lista_super[indice]
+    # Imprimo la sub-lista con formato
     print(f"Producto: {elemento[0]}, cantidad: {elemento[1]}")
     indice += 1
 
+"""
+Estas dos lineas:
+    elemento = lista_super[indice]
+    print(f"Producto: {elemento[0]}, cantidad: {elemento[1]}")
+
+Se pueden resumir en una sola línea:
+    print(f"Producto: {lista_super[indice][0]}, cantidad: {lista_super[indice
+"""
+
+
+# Como sugirieron en clase, anidamos 2 bucles while
+# El primer while itera los elementos de la lista_super
+# El segundo while itera los elementos de la sublista
 
 indice_lista = 0  # declaro el contador / indice
 while indice_lista < len(lista_super):  # len(lista_super) => 2
@@ -66,10 +80,10 @@ while indice_lista < len(lista_super):  # len(lista_super) => 2
         indice_sublista += 1
     # incremento el contador de la lista
     indice_lista += 1
-"""
 
-# Introduccion a metodos
+
+# Introduccion a métodos
 lista = ["manxana", "pera", "kiwi"]
 lista[0] = "manzana"
-lista[3] = "banana"
+# lista[3] = "banana" no puedo agregar elemento a la lista de esta manera
 print(lista)
--- Clase 07/b_listas_metodos.py
+++ Clase 07/b_listas_metodos.py
@@ -28,3 +28,21 @@ print(lista)
 
 lista.pop(2)  # remover el último elemento
 print(lista)
+
+
+# mutabilidad e inmutabilidad
+# los tipos de dato int son inmutables
+numero = 1
+numero = 2
+numero = 3
+
+# los tipos de dato lista son mutables
+lista = [1, 2, 3]
+lista.append(4)
+
+# Conclusión
+# en las líneas 36 y 37, cuando asigno los valores 2 y 3 a numero, en realidad se crea una nueva variable
+# y la original luego se eliminar con el Garbage Collector
+
+# en la línea 40 creo la variable lista con los elementos: 1, 2, 3
+# en la línea 41, el método append modifica la misma variable, por ello el concepto de Mutabilidad
