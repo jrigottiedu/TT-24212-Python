538814a377e2e59741977a1668e1d9c25ae84f4d
titojp <titojp2002@hotmail.com>
2024-10-28 18:02:00 -0300

    prep clase 8

---
+++ Clase 08/app.py
@@ -0,0 +1,13 @@
+# Lista para almacenar los productos
+productos = []
+
+
+# Función principal para el sistema de inventario (NO ELIMINAR)
+def main():
+    # AQUÍ PUEDES COMENZAR A DESARROLLAR LA SOLUCIÓN
+    print("")
+
+
+# Ejecución de la función main() - (NO ELIMINAR)
+if __name__ == "__main__":
+    main()
