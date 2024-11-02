d39f506c72d8cce55012beccbe9bee0a2372bf7a
Juan Pablo Rigotti <juan.rigotti@bue.edu.ar>
2024-11-02 14:11:03 -0300

    ejercicio 8

---
+++ Clase 08/Ejercicios/Ejercicio_1.py
@@ -0,0 +1,20 @@
+"""
+Clase 08 - Ejercicio 1
+
+Registro de ventas por día
+
+Desarrollá un programa que permita registrar las ventas diarias de un comercio durante 5 días. 
+Al finalizar, el sistema debe mostrar el total de ventas realizadas en cada día y el promedio de ventas.
+
+
+
+Tips: 
+
+Usá un bucle while que permita al usuario ingresar el monto de las ventas diarias.
+
+Asegurate de validar que el monto ingresado sea un valor positivo.
+
+Usá un acumulador para la suma de las ventas.
+
+
+"""
---
+++ Clase 08/Ejercicios/Ejercicios_2.py
@@ -0,0 +1,28 @@
+"""
+Actualización del inventario a partir de un arreglo
+
+En una tienda, es necesario actualizar el inventario cuando se venden productos. 
+A continuación, te proporcionamos un arreglo con una lista de productos, 
+donde cada producto tiene un código, una descripción y una cantidad en stock. 
+Escribí un programa que permita:
+
+
+Seleccionar un producto a partir de su código.
+
+Ingresar la cantidad vendida (que debe ser mayor que cero).
+
+Actualizar la cantidad en stock de ese producto restando la cantidad vendida.
+"""
+
+productos = [
+    ["001", "Camiseta básica", 25],
+    ["002", "Pantalón vaquero", 15],
+    ["003", "Chaqueta de cuero", 8],
+    ["004", "Sudadera con capucha", 12],
+    ["005", "Vestido floral", 20],
+    ["006", "Zapatos deportivos", 30],
+    ["007", "Sombrero de paja", 10],
+    ["008", "Camisa de cuadros", 18],
+    ["009", "Cinturón de cuero", 14],
+    ["010", "Bufanda de lana", 22],
+]
