bcd2cc53f737f63cff1b276dc203c01c5421aee3
Juan Pablo Rigotti <juan.rigotti@bue.edu.ar>
2024-12-11 22:38:37 -0300

    updated after class 15

--- entregaFinal/funciones_validacion.py
+++ entregaFinal/funciones_validacion.py
@@ -1,3 +1,12 @@
+"""
+validacion_get_nombre()
+
+1. Solicita el usuario que ingrese el nombre del producto
+2. No se admite dato nulo
+3. El nombre puede contener cualquier caracter
+"""
+
+
 def validacion_get_nombre():
     while True:
         nombre = input("Nombre: ").strip()
@@ -7,10 +16,28 @@ def validacion_get_nombre():
             return nombre
 
 
+"""
+validacion_get_descripcion()
+
+1. Solicita el usuario que ingrese la descripción del producto
+2. Se admite dato nulo
+3. La descripción puede contener cualquier caracter
+"""
+
+
 def validacion_get_descripcion():
     while True:
         descripcion = input("Descripción: ").strip()
         return descripcion
+
+
+"""
+validacion_get_categoria()
+
+1. Solicita el usuario que ingrese la categoria del producto
+2. No se admite dato nulo
+3. La categoría puede contener cualquier caracter
+"""
 
 
 def validacion_get_categoria():
@@ -22,10 +49,19 @@ def validacion_get_categoria():
             return categoria
 
 
+"""
+validacion_get_cantidad()
+
+1. Solicita el usuario que ingrese la cantidad del producto
+2. No se admite dato nulo
+3. La categoría debe ser entero
+"""
+
+
 def validacion_get_cantidad():
     while True:
         try:
-            cantidad = int(input("Cantidad: "))
+            cantidad = int(input("Cantidad: ").strip())
             if not cantidad:
                 print("No se admite dato nulo. Ingrese la cantidad: ")
             else:
@@ -33,6 +69,15 @@ def validacion_get_cantidad():
 
         except ValueError:
             print("Tipo de dato no valido. Ingrese la cantidad: ")
+
+
+"""
+validacion_get_precio()
+
+1. Solicita el usuario que ingrese el precio del producto
+2. No se admite dato nulo
+3. El precio debe ser entero o float
+"""
 
 
 def validacion_get_precio():
